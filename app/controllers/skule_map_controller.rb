6b72b2492924519b70381b39768eeda4b2f0c2c2
Hassan Farooq <hassan.typon@gmail.com>
2009-07-15 18:10:55 -0400

    ok

--- app/controllers/skule_map_controller.rb
+++
@@ -1,9 +0,0 @@
-class SkuleMapController < ApplicationController
-  def showMap
-    #render :text => "<h1>Maps</h1>"
-    @header = "<h2>Maps Page</h2><br /><br /><p>Campus</p>"
-    @location = params[:location]
-    address = {'SF' => "15 King's College Rd", 'BA' => "St George St",}
-    @header = address[@location] || "nonExistant"
-  end
-end
--- app/helpers/skule_map_helper.rb
+++
@@ -1,2 +0,0 @@
-module SkuleMapHelper
-end

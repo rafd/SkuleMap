fe3bd3e30ff605219535a3dbd7a93395a5f84aa6
Hassan Farooq <hassan.typon@gmail.com>
2009-07-27 22:10:34 -0400

    added tooltips, not working completely yet sadly

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,6 +8,9 @@ var startZoom = 15;
 var markerHash={};
 var gmarkers = [];
 
+
+var gah = false;//keeps track of the infowindow
+
 var currentFocus=false;
 
 
@@ -16,11 +19,26 @@ function addMarker(latitude, longitude, id, address, code, name) {
     var gicons = [];
   //  gicons["building"] = new GIcon(G_DEFAULT_ICON, "icon-building.png");
    
+   
+  
+    
+   //creating the marker
     var marker = new GMarker(new GLatLng(latitude, longitude), gicons["buildings"]);
+    //adding the custom tooltip
+    var tooltip = new Tooltip(marker,x[1].innerHTML,1); 
+    marker.tooltip = tooltip; 
+    map.addOverlay(tooltip);
+
     
     
+    
+    
+    //adding the marker to the map and adding it to global array
     map.addOverlay(marker);
+    
     gmarkers.push (marker);
+    
+    //making tabs for the marker infowindow
     var infoTabs = [
     
     new GInfoWindowTab("Info", 
@@ -46,19 +64,39 @@ function addMarker(latitude, longitude, id, address, code, name) {
 
 
 
-
+//adding click event that launches infowindow
 		GEvent.addListener(marker,'click',function() {
     
     map.panTo (marker.getLatLng());
 
     marker.openInfoWindowTabsHtml(infoTabs, {
     selectedTab:0, maxWidth:800});
-    
+    var infoWindow = map.getInfoWindow();
+		gah = true;
+		
 		});
-
+		
+		    
+    //adding mouseover and mouseout events to show tooltip
+    GEvent.addListener(marker,'infowindowclose',function(){
+    gah=false;
+    }); 
     
     
-    
+    GEvent.addListener(marker,'mouseover',function(){
+       		
+       	if ((!gah) || !(this.isHidden)) {
+       	
+       		this.tooltip.show();     	}
+       		
+    }); 
+   
+    GEvent.addListener(marker,'mouseout',function(){ 
+    	
+    	
+    	this.tooltip.hide(); 
+    	
+    	});
 }
 
 
@@ -66,14 +104,26 @@ function init() {
         map = new GMap($("map"));
         map.addControl(new GSmallMapControl());
         map.setCenter(new GLatLng(centerLatitude, centerLongitude), startZoom);
+        
         for(i=0;i<markers.length; i++) {
+			  
+          
+          
+          
+          
           var current =markers[i];
           //marker=addMarker(current.lat, current.lng, current.id, current.address, current.code, current.name);
           marker=addMarker(current.building.lat, current.building.lng, current.building.id, current.building.address, current.building.code, current.building.name);
           
           markerHash[current.id]={marker:marker,address:current.address,visible:true};
+          
+         
+}
+          
+          
+          
         }
- }
+ 
 
 
 function boxclick(box,i) {
@@ -84,12 +134,32 @@ function boxclick(box,i) {
 					else {
 					gmarkers[i-1 ].hide();
 					        map.closeInfoWindow(gmarkers[i-1]);
+					        				gmarkers[i-1].tooltip.hide();
 					} 
 }
 
 function liclick(li,i) {
-			map.panTo (gmarkers[i-1].getLatLng());
-  
+
+			if (!(gmarkers[i-1].isHidden())){
+			
+				map.panTo (gmarkers[i-1].getLatLng());
+			}
+}
+
+function limouseover (li,i) {
+			
+			if (!(gmarkers[i-1].isHidden())  && !gah){
+				gmarkers[i-1].tooltip.show();
+				
+  		}
+  		          
+}
+function limouseout (li,i) {
+			
+		
+				gmarkers[i-1].tooltip.hide();
+				
+  	 		          
 } 
 
     

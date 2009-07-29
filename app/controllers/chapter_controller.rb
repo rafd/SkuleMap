20337ba097599fbeccc6641fc541df083c22388e
KaiZou <kaizou898@hotmail.com>
2009-07-28 20:37:33 -0400

    search box prototype; redenering partial not working; should make this client side later

--- app/controllers/chapter_controller.rb
+++ app/controllers/chapter_controller.rb
@@ -11,5 +11,15 @@ render :text=>(Building.find :all).to_json
 
 end
 
+def search
+  search_term = params[:keywords]
+  @result_buildings = Building.find_by_sql("SELECT * FROM buildings WHERE " +
+                                          "name LIKE '%#{search_term}%' OR " +
+                                          "code LIKE '%#{search_term}%' OR " +
+                                          "address LIKE '%#{search_term}%' OR " +
+                                          "zip_code LIKE '%#{search_term}%' OR " +
+                                          "building_type LIKE '%#{search_term}%'")
+  render :action => "map"
+end
 
 end
